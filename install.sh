919d6111ee5dd81530bf5891709c431824e5824b
RainoXu <tblonggang@gmail.com>
2013-03-28 14:16:56 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ln -s $(pwd)/.gvimrc ~/.vimrc
-echo "已将 $(pwd)/.gvimrc 复制到 ~/.vimrc"
+ln -s $(pwd)/vim/.gvimrc ~/.vimrc
+echo "已将 $(pwd)/vim/.gvimrc 复制到 ~/.vimrc"
 
-ln -s $(pwd)/.gvimrc ~/.gvimrc
-echo "已将 $(pwd)/.gvimrc 复制到 ~/.gvimrc"
+ln -s $(pwd)/vim/.gvimrc ~/.gvimrc
+echo "已将 $(pwd)/vim/.gvimrc 复制到 ~/.gvimrc"
 
-ln -s $(pwd)/vim/ ~/.vim
-echo "已将 $(pwd)/vim 复制到 ~/.vim"
+#ln -s $(pwd)/vim ~/.vim
+#echo "已将 $(pwd)/vim 复制到 ~/.vim"
