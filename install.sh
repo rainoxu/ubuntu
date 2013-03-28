c8f813b9e01cf197a884c6cd20c636cf50d2a338
RainoXu <rainoxu@t-rainoxu.(none)>
2013-03-28 14:01:25 +0800

    update install file

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+ln -s $(pwd)/.gvimrc ~/.vimrc
+echo "已将 $(pwd)/.gvimrc 复制到 ~/.vimrc"
+
 ln -s $(pwd)/.gvimrc ~/.gvimrc
 echo "已将 $(pwd)/.gvimrc 复制到 ~/.gvimrc"
+
+ln -s $(pwd)/vim/ ~/.vim
+echo "已将 $(pwd)/vim 复制到 ~/.vim"
